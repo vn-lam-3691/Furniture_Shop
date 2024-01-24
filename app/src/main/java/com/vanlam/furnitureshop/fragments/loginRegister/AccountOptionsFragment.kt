947a1047696f98ff4c29900d99a0b6ff2b69dab5
VanLamTruong <lamtruong3242@gmail.com>
2024-01-24 22:01:13 +0700

    design Introduction fragment

---
+++ app/src/main/java/com/vanlam/furnitureshop/fragments/loginRegister/AccountOptionsFragment.kt
@@ -0,0 +1,8 @@
+package com.vanlam.furnitureshop.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.vanlam.furnitureshop.R
+
+class AccountOptionsFragment : Fragment(R.layout.fragment_account_options) {
+
+}
---
+++ app/src/main/java/com/vanlam/furnitureshop/fragments/loginRegister/LoginFragment.kt
@@ -0,0 +1,8 @@
+package com.vanlam.furnitureshop.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.vanlam.furnitureshop.R
+
+class LoginFragment : Fragment(R.layout.fragment_login) {
+
+}
