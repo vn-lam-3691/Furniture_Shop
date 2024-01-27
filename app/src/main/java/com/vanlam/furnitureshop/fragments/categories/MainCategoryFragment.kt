49141af26ce6c2bd8ea30811f28253e0088e4c2f
VanLamTruong <lamtruong3242@gmail.com>
2024-01-27 17:21:00 +0700

    design MainCategory fragment

--- app/src/main/java/com/vanlam/furnitureshop/fragments/categories/MainCategoryFragment.kt
+++ app/src/main/java/com/vanlam/furnitureshop/fragments/categories/MainCategoryFragment.kt
@@ -3,6 +3,6 @@ package com.vanlam.furnitureshop.fragments.categories
 import androidx.fragment.app.Fragment
 import com.vanlam.furnitureshop.R
 
-class MainCategoryFragment : Fragment(R.layout.fragment_main) {
+class MainCategoryFragment : Fragment(R.layout.fragment_main_category) {
 
 }
