94067f88a89ddc611d2b2e2feaf7b4683f0d9a93
VanLamTruong <lamtruong3242@gmail.com>
2024-01-27 17:46:36 +0700

    create module app Adder

---
+++ furnitureadder/src/main/java/com/vanlam/furnitureadder/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.vanlam.furnitureadder
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.vanlam.furnitureadder.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+}
