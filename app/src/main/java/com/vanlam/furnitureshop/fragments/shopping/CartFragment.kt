35c8504190b6c508b2017ba76c5dacee252777cb
VanLamTruong <lamtruong3242@gmail.com>
2024-02-20 21:21:40 +0700

    feat: load products in cart, in/decrease quantity, delete item

--- app/src/main/java/com/vanlam/furnitureshop/fragments/shopping/CartFragment.kt
+++ app/src/main/java/com/vanlam/furnitureshop/fragments/shopping/CartFragment.kt
@@ -1,8 +1,148 @@
 package com.vanlam.furnitureshop.fragments.shopping
 
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.vanlam.furnitureshop.R
+import com.vanlam.furnitureshop.adapters.CartProductAdapter
+import com.vanlam.furnitureshop.databinding.FragmentCartBinding
+import com.vanlam.furnitureshop.firebase.FirebaseCommon
+import com.vanlam.furnitureshop.utils.Resource
+import com.vanlam.furnitureshop.utils.VerticalItemDecoration
+import com.vanlam.furnitureshop.viewmodel.CartViewModel
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collectLatest
 
-class CartFragment: Fragment(R.layout.fragment_cart) {
+@AndroidEntryPoint
+class CartFragment: Fragment() {
+    private lateinit var binding: FragmentCartBinding
+    private val cartAdapter by lazy { CartProductAdapter() }
+    private val viewModel by activityViewModels<CartViewModel>()
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentCartBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupCartRcv()
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.cartProducts.collectLatest {
+                when (it) {
+                    is Resource.Loading -> {
+                        binding.progressbarCart.visibility = View.VISIBLE
+                    }
+                    is Resource.Success -> {
+                        binding.progressbarCart.visibility = View.INVISIBLE
+
+                        if (it.data!!.isEmpty()) {
+                            showEmptyCart()
+                            hideOtherView()
+                        }
+                        else {
+                            hideEmptyCart()
+                            showOtherView()
+                            cartAdapter.differ.submitList(it.data)
+                        }
+                    }
+                    is Resource.Error -> {
+                        binding.progressbarCart.visibility = View.INVISIBLE
+                        Toast.makeText(requireContext(), it.message.toString(), Toast.LENGTH_SHORT).show()
+                    }
+                    else -> Unit
+                }
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.totalPriceCart.collectLatest { price ->
+                price?.let {
+                    binding.tvTotalPrice.text = "$ ${String.format("%.2f", it)}"
+                }
+            }
+        }
+
+        lifecycleScope.launchWhenStarted {
+            viewModel.deleteCartProduct.collectLatest {
+                val alertDialog = AlertDialog.Builder(requireContext()).apply {
+                    setTitle("Delete item")
+                    setMessage("Do you want to delete this item from your cart?")
+                    setNegativeButton("No", DialogInterface.OnClickListener { dialogInterface, _ ->
+                        dialogInterface.dismiss()
+                    })
+                    setPositiveButton("Yes", DialogInterface.OnClickListener { dialogInterface, _ ->
+                        viewModel.deleteItemFromCart(it)
+                        dialogInterface.dismiss()
+                    })
+                }
+                alertDialog.create()
+                alertDialog.show()
+            }
+        }
+
+        cartAdapter.onClick = {
+            val bundle = Bundle().apply { putParcelable("product", it.product) }
+            findNavController().navigate(R.id.action_cartFragment_to_productDetailFragment, bundle)
+        }
+
+        cartAdapter.onIncreaseClick = {
+            viewModel.changeQuantity(it, FirebaseCommon.QuantityChanging.INCREASE)
+        }
+
+        cartAdapter.onDecreaseClick = {
+            viewModel.changeQuantity(it, FirebaseCommon.QuantityChanging.DECREASE)
+        }
+    }
+
+    private fun showOtherView() {
+        binding.apply {
+            rcvCartProduct.visibility = View.VISIBLE
+            totalBoxContainer.visibility = View.VISIBLE
+            buttonCheckout.visibility = View.VISIBLE
+        }
+    }
+
+    private fun hideOtherView() {
+        binding.apply {
+            rcvCartProduct.visibility = View.GONE
+            totalBoxContainer.visibility = View.GONE
+            buttonCheckout.visibility = View.GONE
+        }
+    }
+
+    private fun hideEmptyCart() {
+        binding.apply {
+            layoutCartEmpty.visibility = View.GONE
+        }
+    }
+
+    private fun showEmptyCart() {
+        binding.apply {
+            layoutCartEmpty.visibility = View.VISIBLE
+        }
+    }
+
+    private fun setupCartRcv() {
+        binding.rcvCartProduct.apply {
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+            adapter = cartAdapter
+            addItemDecoration(VerticalItemDecoration())
+        }
+    }
 }
--- app/src/main/java/com/vanlam/furnitureshop/viewmodel/CartViewModel.kt
+++ app/src/main/java/com/vanlam/furnitureshop/viewmodel/CartViewModel.kt
@@ -7,10 +7,14 @@ import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.FirebaseFirestore
 import com.vanlam.furnitureshop.data.CartProduct
 import com.vanlam.furnitureshop.firebase.FirebaseCommon
+import com.vanlam.furnitureshop.helper.getProductPrice
 import com.vanlam.furnitureshop.utils.Resource
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
@@ -25,8 +29,26 @@ class CartViewModel @Inject constructor(
     val cartProducts = _cartProducts.asStateFlow()
     private var cartProductDocument = emptyList<DocumentSnapshot>()
 
+    private val _deleteCartProduct = MutableSharedFlow<CartProduct>()
+    val deleteCartProduct = _deleteCartProduct.asSharedFlow()
+
     init {
         getCartProducts()
+    }
+
+    val totalPriceCart = cartProducts.map {
+        when (it) {
+            is Resource.Success -> {
+                calculatorTotal(it.data!!)
+            }
+            else -> null
+        }
+    }
+
+    private fun calculatorTotal(data: List<CartProduct>): Float {
+        return data.sumByDouble {  cartProduct ->
+            (cartProduct.product.offerPercentage.getProductPrice(cartProduct.product.price) * cartProduct.quantity).toDouble()
+        }.toFloat()
     }
 
     private fun getCartProducts() {
@@ -52,9 +74,16 @@ class CartViewModel @Inject constructor(
             val documentId = cartProductDocument[index].id
             when (quantityChanging) {
                 FirebaseCommon.QuantityChanging.INCREASE -> {
+                    viewModelScope.launch { _cartProducts.emit(Resource.Loading()) }
                     increaseQuantity(documentId)
                 }
                 FirebaseCommon.QuantityChanging.DECREASE -> {
+                    if (cartProduct.quantity == 1) {
+                        viewModelScope.launch { _deleteCartProduct.emit(cartProduct) }
+                        return
+                    }
+
+                    viewModelScope.launch { _cartProducts.emit(Resource.Loading()) }
                     decreaseQuantity(documentId)
                 }
             }
@@ -74,6 +103,17 @@ class CartViewModel @Inject constructor(
             if (e != null) {
                 viewModelScope.launch { _cartProducts.emit(Resource.Error(e.message.toString())) }
             }
+        }
+    }
+
+    fun deleteItemFromCart(cartProduct: CartProduct) {
+        val index = cartProducts.value.data?.indexOf(cartProduct)
+
+        if (index != null && index != -1) {
+            val documentId = cartProductDocument[index].id
+            firestore.collection("user").document(auth.uid!!)
+                .collection("cart").document(documentId)
+                .delete()
         }
     }
 }
